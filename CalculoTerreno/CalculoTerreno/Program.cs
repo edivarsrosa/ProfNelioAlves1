62521d9eb08b53ff2c126e4f9ad1dfa5faf74b63
edivarsrosa <edivarsrosa@gmail.com>
2021-04-28 05:32:43 -0300

    alterações nos arquivos

---
+++ CalculoTerreno/CalculoTerreno/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoTerreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OLA. VAMOS INSERIR OS DADOS DO SEU TERRENO ");
+            Console.WriteLine();
+            Console.WriteLine("DIGITE LARGURA: ");
+            double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("DIGITE O COMPRIMENTO: ");
+            double comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("DIGITE O VALOR DO METRO QUADRADO: ");
+            double precometro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = largura * comprimento;
+            double preco = area * precometro;
+
+            Console.WriteLine("ÁREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PREÇO = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Fim do Programa.");
+        }
+    }
+}
---
+++ FormulaDeBascara/FormulaDeBascara/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FormulaDeBascara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            bool delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            bool x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            bool x2 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
